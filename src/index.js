13d3fd45408d584d6934ec2c8f66079991b5c9dd
bajakprashant1798 <bajakprashant1798@gmail.com>
2024-04-19 01:26:45 +0530

    util code - apierror api response

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on('error', () => {
+        console.log("Error", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000), () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    }
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed!", err);
+})
 
 
 /*
